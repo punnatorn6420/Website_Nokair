098d971eb0b08022c58a48be1e4e32ce3af10d9b
punnatornkpp <96907872+punnatorn6420@users.noreply.github.com>
2025-08-26 16:56:02 +0700

    chore: set up project structure

---
+++ src/components/travel/TravelGuides.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TravelGuides: React.FC = () => {
+  return (
+    <section>
+      <h2>Travel Guides</h2>
+    </section>
+  );
+};
+
+export default TravelGuides;
